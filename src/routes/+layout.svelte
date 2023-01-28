5a7fa4ae5bd7384c6c17d8cc96fd1968eab50b84
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2023-01-28 22:26:27 +0100

    start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="px-[5vw]">
+	<nav>
+		<ul class="flex justify-around">
+			<li>logo</li>
+			<li>zorobabel</li>
+			<li>films</li>
+			<li>appels à projet calls for proposal</li>
+			<li>ateliers workshops</li>
+			<li>événements events</li>
+			<li>zorobabel</li>
+			<li>search</li>
+		</ul>
+	</nav>
+	<section id="content">
+		<slot />
+	</section>
+</main>
