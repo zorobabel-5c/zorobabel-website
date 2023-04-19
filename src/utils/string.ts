c57709e0e2b148632840262ebdf81d02fa9a37ff
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2023-04-19 23:24:02 +0200

    more

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -8,9 +8,9 @@ export function truncate(str: string, len = 30): string {
 export function removePrefix(str: string, prefix: string): string {
 	const [str1, prefix1] = [str, prefix].map((s) => s.replaceAll('’', "'"));
 	if (str1.startsWith(prefix1)) {
-		return str1.slice(prefix1.length).replace(/^\W+/, '');
+		return '...' + str1.slice(prefix1.length).replace(/^\W+/, '');
 	} else if (/[aeiou]$/.test(prefix1) && str1.startsWith(prefix1.slice(0, -1))) {
-		return str1.slice(prefix1.length).replace(/^\W+/, '');
+		return '...' + str1.slice(prefix1.length).replace(/^\W+/, '');
 	}
 	return str1;
 }
