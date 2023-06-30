f6a6a622e7368b92d9494d5eab88baf8a3148f78
rui hildt <rui@mullvad.net>
2023-06-30 17:39:00 +0200

    Add evenements page

---
+++ src/lib/components/static/EventsLayout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import StaticLayout from '$lib/components/static/StaticLayout.svelte';
+</script>
+
+<StaticLayout>
+	<svelte:fragment slot="sidemenu">
+		<li>
+			<a href="stages" class:text-red-500={$page.url.pathname.startsWith('/événements')}>
+				<p>événements</p>
+			</a>
+		</li>
+	</svelte:fragment>
+	<div slot="content">
+		<slot name="content" />
+	</div>
+</StaticLayout>
--- src/routes/événements/+page.svelte
+++ src/routes/événements/+page.svelte
@@ -1,1 +1,39 @@
-<h1>Under Development</h1>
+<script lang="ts">
+	import { page } from '$app/stores';
+	import StaticLayout from '$lib/components/static/StaticLayout.svelte';
+	import { imageFromAssets } from '$lib/utils';
+	import type { LayoutData } from './$houdini';
+
+	export let data: LayoutData;
+
+	$: ({ EventsQuery } = data);
+	$: ({ evenements = [] } = $EventsQuery.data! ?? {});
+</script>
+
+<StaticLayout>
+	<svelte:fragment slot="sidemenu">
+		<li>
+			<a href="événements" class:text-red-500={$page.url.pathname.startsWith('événements')}>
+				<p>événements</p>
+			</a>
+		</li>
+	</svelte:fragment>
+	<div slot="content">
+		{#if !$EventsQuery.fetching}
+			{#each evenements as item}
+				<div class="pb-8">
+					<img
+						src={imageFromAssets(item.affiche) + '?width=770&quality=30'}
+						alt="todo"
+						class="w-full object-cover"
+					/>
+					<h1>
+						{item.titre}
+					</h1>
+					<p>{@html item.description}</p>
+					<p>{item.date_de_peremption}</p>
+				</div>
+			{/each}
+		{/if}
+	</div>
+</StaticLayout>
