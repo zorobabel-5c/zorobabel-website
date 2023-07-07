87e63a661b7492359f77c9f9b785538612f8f628
rui hildt <rui@mullvad.net>
2023-07-07 15:34:18 +0200

    Update packages

--- src/client.ts
+++ src/client.ts
@@ -1,23 +1,5 @@
-import { HoudiniClient, type RequestHandler } from '$houdini';
+import { HoudiniClient } from '$houdini';
 
-const requestHandler: RequestHandler = async ({
-	fetch,
-	text = '',
-	variables = {},
-	metadata
-}) => {
-	const url = 'https://backend-zorobabel.5c.be/graphql';
-	const result = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			query: text,
-			variables
-		})
-	});
-	return await result.json();
-}
-
-export default new HoudiniClient(requestHandler);
+export default new HoudiniClient({
+	url: 'https://backend-zorobabel.5c.be/graphql'
+});
