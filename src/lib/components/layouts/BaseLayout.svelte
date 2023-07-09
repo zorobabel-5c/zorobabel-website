b52c6c25a9228646a40d92a73f273a3303afea3f
rui hildt <rui@mullvad.net>
2023-07-09 23:55:58 +0200

    Adjust BasicLayout responsiveness

--- src/lib/components/layouts/BaseLayout.svelte
+++ src/lib/components/layouts/BaseLayout.svelte
@@ -1,17 +1,17 @@
-<section id="zorobabel" class="grid lg:grid-cols-[146px_1fr]">
-	<aside
-		class="grid lg:grid-cols-[1fr] md:lg:grid-cols-[1fr] lg:border-none md:border-r border-solid border-gray-800 md:mr-4 font-josefin"
+<section id="zorobabel" class="grid md:grid-cols-[146px_1fr]">
+	<nav
+		class="col-span-2 md:col-span-1 font-josefin mx-4 md:sticky md:top-[101px] md:h-[calc(97vh-90px)] md:border-r border-solid border-gray-800 text-center"
+		data-sveltekit-preload-data="tap"
 	>
-		<nav
-			class="sidenav lg:border-r border-solid border-gray-800 text-center"
-			data-sveltekit-preload-data="tap"
+		<ul
+			class="[&>li]:mb-3 md:[&>li]:mb-12 flex flex-row md:flex-col flex-wrap [&>li]:basis-1/4 [&>li]:grow justify-between items-center"
 		>
-			<ul class="[&>li]:mb-12  lg:h-[calc(100vh-120px)] md:h-[calc(50vh-60px)]">
-				<slot name="sidemenu" />
-			</ul>
-		</nav>
-	</aside>
-	<main class="[&_p]:pb-5 [&_a]:text-red-500 text-sm [&_h1]:font-bold [&_h2]:font-bold">
+			<slot name="sidemenu" />
+		</ul>
+	</nav>
+	<main
+		class="col-span-2 md:col-span-1 overflow-y-scroll mx-4 [&_p]:pb-5 [&_a]:text-red-500 text-sm [&_h1]:font-bold [&_h2]:font-bold"
+	>
 		<slot name="content" />
 	</main>
 </section>
