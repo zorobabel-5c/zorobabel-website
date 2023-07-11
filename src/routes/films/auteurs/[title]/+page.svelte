ac7e321744a4d8b8883e1417ce9adbdaec3f87b5
rui hildt <rui@mullvad.net>
2023-07-11 10:23:17 +0200

    Add a gallery modal for film's images

--- src/routes/films/auteurs/[title]/+page.svelte
+++ src/routes/films/auteurs/[title]/+page.svelte
@@ -5,12 +5,39 @@
 
 	import VimeoIframe from '$lib/components/VimeoIframe.svelte';
 	import PageHead from '$lib/components/PageHead.svelte';
+	import AngleRight from '$lib/components/icons/AngleRight.svelte';
+	import AngleLeft from '$lib/components/icons/AngleLeft.svelte';
+	import Close from '$lib/components/icons/Close.svelte';
 
 	export let data: LayoutData;
 
 	$: ({ FilmDAuteurs } = data);
 	$: ({ films = [] } = $FilmDAuteurs.data! ?? {});
 	$: currentFilm = films.find((f) => f.slug === $page.params.title);
+
+	let showGalleryModal = false;
+	let currentIndex: number;
+
+	const handleImage = (event: Event) => {
+		currentIndex = event.target?.dataset.listIndex;
+		showGalleryModal = true;
+	};
+
+	$: images = [
+		currentFilm?.image_1,
+		currentFilm?.image_2,
+		currentFilm?.image_3,
+		currentFilm?.affiche
+	];
+
+	$: prev = () => {
+		currentIndex--;
+	};
+	$: next = () => {
+		currentIndex++;
+	};
+
+	$: console.log(currentIndex);
 </script>
 
 <PageHead head={currentFilm?.titre_original} />
@@ -18,9 +45,8 @@
 {#if $FilmDAuteurs.data}
 	<section class="mm:overflow-y-scroll h-[calc(100vh-120px)]">
 		<h1 class="text-center font-josefin uppercase">
-			<span>{currentFilm?.titre_original} {currentFilm?.titre_english ? '-' : ''} </span><span
-				class="italic font-light">{currentFilm?.titre_english}</span
-			>
+			<span>{currentFilm?.titre_original} {currentFilm?.titre_english ? '-' : ''} </span>
+			<span class="italic font-light">{currentFilm?.titre_english}</span>
 		</h1>
 		<div class="text-center">
 			<span>{currentFilm?.realisation}</span>
@@ -44,19 +70,28 @@
 				<p>{currentFilm?.prix_obtenus_par_le_film}</p>
 			</div>
 		{/if}
-		<div class="grid grid-cols-3 mt-4">
+		<div class="grid grid-cols-3 mt-4 cursor-pointer">
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<img
-				src={imageFromAssets(currentFilm?.image_1) + '?width=270&quality=30'}
+				on:click={handleImage}
+				src={imageFromAssets(images[0]) + '?width=270&quality=30'}
 				class="object-cover h-full"
 				alt="image 1 du film '{currentFilm?.titre_original}'"
+				data-list-index={0}
 			/>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<img
-				src={imageFromAssets(currentFilm?.image_2) + '?width=270&quality=30'}
+				on:click={handleImage}
+				data-list-index={1}
+				src={imageFromAssets(images[1]) + '?width=270&quality=30'}
 				class="object-cover h-full"
 				alt="image 2 du film '{currentFilm?.titre_original}'"
 			/>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<img
-				src={imageFromAssets(currentFilm?.image_3) + '?width=270&quality=30'}
+				on:click={handleImage}
+				data-list-index={2}
+				src={imageFromAssets(images[2]) + '?width=270&quality=30'}
 				class="object-cover h-full"
 				alt="image 3 du film '{currentFilm?.titre_original}'"
 			/>
@@ -108,11 +143,58 @@
 				<span>{currentFilm?.ville} - {currentFilm?.annee}</span>
 			</li>
 			<li>
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
 				<img
-					src={imageFromAssets(currentFilm?.affiche) + '?width=800&quality=30'}
+					on:click={handleImage}
+					data-list-index={3}
+					src={imageFromAssets(images[3]) + '?width=800&quality=30'}
 					alt="affiche du film '{currentFilm?.titre_original}'"
+					class="cursor-pointer"
 				/>
 			</li>
 		</ul>
 	</section>
+	{#if showGalleryModal}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="w-full h-[100vh] bg-[rgb(0,0,0,0.9)] fixed top-0 left-0 z-50 grid place-content-center"
+			on:click|self={() => (showGalleryModal = false)}
+		>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div
+				on:click={() => (showGalleryModal = false)}
+				class="op-8 right-[5vw] absolute z-20 cursor-pointer"
+			>
+				<Close classes="text-white w-7 hover:text-red-500" />
+			</div>
+			<div
+				class="lg:w-[700px] md:w-[90vw] flex gap-4 [&>button]:self-center [&>button]:p-2 [&>button]:rounded-lg"
+			>
+				<button
+					on:click={prev}
+					class:invisible={currentIndex === 0}
+					class="text-white hover:text-red-500"
+				>
+					<AngleLeft classes="w-5 lg:w-7" />
+				</button>
+				<div class="flex-1">
+					<img
+						src={imageFromAssets(images[currentIndex]) + '?width=800&quality=30'}
+						alt="affiche du film '{currentFilm?.titre_original}'"
+						class="object-contain"
+					/>
+					<p class="text-white text-center">
+						{images[currentIndex]?.title}
+					</p>
+				</div>
+				<button
+					on:click={next}
+					class:invisible={currentIndex === 3}
+					class="text-white hover:text-red-500"
+				>
+					<AngleRight classes="w-5 lg:w-7" />
+				</button>
+			</div>
+		</div>
+	{/if}
 {/if}
--- todo.md
+++ todo.md
@@ -5,8 +5,6 @@
 
 # films
 
-- When clicking on the 3 images films, display them in a gallery modal in a bigger size
-- auteurs: update filtering logic
 - scroll up when clicking on another film from the list
 - if a film is no available => display trailer, if trailer not available => display the backup image, if no backup image available => display one of the 3 images => else don't display anything.
 - open by default with the latet finished film
@@ -19,9 +17,3 @@
 # BONUS
 
 - [ ] BONUS Find a way to create unique links for events
-
-Autre chose avant que je ne continue à investiguer (je sais que c'est compliqué mais c'est quand même expliqué dans le cahier des charges) :
-
-- tous les films d'auteur, y compris ceux qui ne sont pas terminés apparaissent dans l'onglet films d'auteur,
-- tout comme il apparaissent pour l'instant dans la page catalogue.
-- c'est juste sur la page d'accueil que les films non-terminés n'apparaissent pas.
