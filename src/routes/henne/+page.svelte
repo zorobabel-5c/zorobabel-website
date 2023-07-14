ce6af3474fde6c9fb9e3dd83454c84d5464a56f5
rui hildt <rui@mullvad.net>
2023-07-14 14:05:12 +0200

    Add redirects for external websites

---
+++ src/routes/henne/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+
+	$: browser && goto(`https://henne.zorobabel.be`);
+</script>
---
+++ src/routes/sonder/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+
+	$: browser && goto(`https://sonder.zorobabel.be`);
+</script>
