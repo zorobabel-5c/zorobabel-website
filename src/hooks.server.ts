2cd30de1a7b83eddd6db6ff4ead0cba9e309481f
rui hildt <rui@mullvad.net>
2023-08-02 17:51:07 +0200

    Handle the mysterious "/favicon.ico" request

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,9 @@ export async function handle({ event, resolve }) {
 	if (event.url.pathname.startsWith('/health')) {
 		return new Response();
 	}
+	if (event.url.pathname.startsWith('/favicon.ico')) {
+		return new Response();
+	}
 
 	const response = await resolve(event);
 	return response;
